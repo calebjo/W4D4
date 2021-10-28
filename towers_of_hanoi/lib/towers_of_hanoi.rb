444e2d72f03c928ac6bc0bb7ecd0a11661e89cc2
calebjo <caleb.ri.jones@gmail.com>
2021-10-28 16:02:03 -0400

    Added methods for towers_of_hanoi.rb

--- towers_of_hanoi/lib/towers_of_hanoi.rb
+++ towers_of_hanoi/lib/towers_of_hanoi.rb
@@ -1,32 +1,58 @@
 class Hanoi
+    attr_accessor :pile1, :pile2, :pile3
+
     def initialize
         @pile1 = [1,2]
         @pile2 = [3,4,5]
         @pile3 = [6,7,8]
+        @game_over = false
     end
 
     def move
         until won?
+            p @pile1
+            p @pile2
+            p @pile3
+
             puts "Select a start pile (1,2,3)"
             start_pile = gets.chomp.to_i
 
-            puts "Select a end pile (1,2,3)"
+            disc = nil
+            case start_pile 
+            when 1
+                disc = @pile1.pop
+            when 2
+                disc = @pile2.pop
+            when 3
+                disc = @pile3.pop
+            end
+
+            puts "Select an end pile (1,2,3)"
             end_pile = gets.chomp.to_i 
 
-            case start_pile
-                
+            case end_pile
             when 1
-                disc == @pile1.pop
+                @pile1.push(disc)
             when 2
-                disc == @pile2.pop
+                @pile2.push(disc)
             when 3
-                disc == @pile3.pop
+                @pile3.push(disc)
             end
+
         end
+        puts "You win!"
+    end
+
+    def valid_arg?(n)
+        n == 1 || n == 2 || n == 3
     end
 
     def won?
+        @game_over = true if eight_count
     end
 
+    def eight_count
+        @pile1.length == 8 || @pile2.length == 8 || @pile3.length == 8 
+    end
 
 end
--- towers_of_hanoi/spec/towers_of_hanoi_spec.rb
+++ towers_of_hanoi/spec/towers_of_hanoi_spec.rb
@@ -6,26 +6,27 @@ RSpec.describe "Hanoi" do
 
     describe "#initialize" do
         it "should create 3 piles of discs" do
-            expect(pile1).to eq([1,2])
-            expect(pile2).to eq([3,4,5])
-            expect(pile3).to eq([6,7,8])
+            expect(hanoi.pile1).to eq([1,2])
+            expect(hanoi.pile2).to eq([3,4,5])
+            expect(hanoi.pile3).to eq([6,7,8])
         end
 
     end
 
     describe "#move" do
         it "should get user input" do
-        expect(start_pile).to_not eq(nil)
-        expect(end_pile).to_not eq(nil)
-    end
-
-    describe "#won?" do
-        it "should check if any pile has 8 anything" do 
-            8_count = pile1.length == 8 || pile2.length == 8 || pile3.length == 8  
-            expect(8_count).to be true
+            # allow($stdin).to receive(:gets)
+            # pile = $stdin.gets
+            # expect(pile).not_to eq(nil)
         end
     end
 
-
+    describe "#won?" do
+        hanoi.pile3 = [1,2,3,4,5,6,7,8]
+        it "should check if any pile has 8 anything" do 
+            # expect(pile3.length).to eq(8)
+            expect(hanoi.won?).to be true
+        end
+    end
 
 end
