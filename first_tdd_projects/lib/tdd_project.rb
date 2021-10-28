8890525841d3635f806b31e10fcc1fbd4e703659
AlexDoes <alexsw.191@gmail.com>
2021-10-28 13:59:45 -0400

    tdd spec 1-3 complete

--- first_tdd_projects/lib/tdd_project.rb
+++ first_tdd_projects/lib/tdd_project.rb
@@ -8,3 +8,28 @@ def my_uniq(array)
     end
     new_arr
 end
+
+class Array
+    def two_sum
+        new_arr = []
+        self.each_with_index do |ele1, i|
+            self.each_with_index  do |ele2, j|
+            new_arr << [i,j] if j > i && ele1 + ele2 == 0
+            end
+        end
+        new_arr
+    end
+end
+
+def my_transpose(arr)
+    col = arr.length
+    new_arr = arr.map {|row| row.map {|ele| ele}}
+    
+    (0...col).each do |i|
+        (0...col).each do |j|
+            arr[i][j] = new_arr[j][i]
+        end
+    end
+
+    arr
+end
--- first_tdd_projects/spec/tdd_project_spec.rb
+++ first_tdd_projects/spec/tdd_project_spec.rb
@@ -9,3 +9,31 @@ describe "#my_uniq" do
         expect(my_uniq(array)).to eq(array2)
     end
 end
+
+describe "two_sum" do 
+    let(:array){[-1,0,2,-2,1]}
+    
+    it "finds pairs that sums to 0" do
+        expect(array.two_sum).to eq([[0,4],[2,3]])
+    end
+
+    it "should have the smaller index first" do
+        expect(array[0] < array[1]).to be true
+    end 
+end
+
+describe "my_transpose" do 
+    let(:array) {    
+    [   [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]   ] }
+
+    let(:array2) {    
+    [   [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]   ] }
+
+    it "should zip the arrays" do
+        expect(my_transpose(array)).to eq(array2)
+    end
+end
